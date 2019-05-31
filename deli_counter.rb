755d684c0d0317c2906fdd5fb736f41971968fb1
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-31 17:58:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+
+def line(katz_deli)
+  greeting = []
+  count = 0
+  while count <= katz_deli.length-1
+    # greeting.push("The line is currently empty.")
+    # count = count + 1
+  end
+  puts "The line is currently empty."
+end
